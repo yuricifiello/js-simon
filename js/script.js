dc3a0cd1242d73706895d4c279f0da641541f235
yuricifiello <yuricifiello@gmail.com>
2025-04-12 16:10:42 +0200

    elements

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// # ELEMENTI DOM
+const countdownElement = document.getElementById("countdown");
+const numbersListElement = document.getElementById("numbers-list");
+const formElement = document.getElementById("answers-form");
+const inputs = document.querySelectorAll("input");
+const messageElement = document.getElementById("message");
+const instructionsElement = document.getElementById("instructions");
+
+let randomNumbers = [];
