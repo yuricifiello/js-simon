e11e9b6883af3cdb9fb53c5f74a3bcfae7ddb09a
yuricifiello <yuricifiello@gmail.com>
2025-04-12 16:11:15 +0200

    generate numbers

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ const messageElement = document.getElementById("message");
 const instructionsElement = document.getElementById("instructions");
 
 let randomNumbers = [];
+
+// GENERO 5 NUMERI DA 1 A 50
+function generateRandomNumbers() {
+  while (randomNumbers.length < 5) {
+    const number = Math.floor(Math.random() * 50) + 1;
+    if (!randomNumbers.includes(number)) {
+      randomNumbers.push(number);
+    }
+  }
+}
